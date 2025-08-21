ae33ccd65cca30acf2c90a9ea59218d64d8e9a1d
vanshika sahu <vanshikasahu281105@gmail.com>
2025-08-21 21:24:15 +0530

    Time: 4 ms (35.97%) | Memory: 44.3 MB (22.35%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,18 +1,37 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
+        Stack<Integer> st = new Stack<>();
         int[] ans = new int[prices.length];
-        for(int i=0;i<prices.length-1;i++){
-            for(int j=i+1;j<prices.length;j++){
-                if(prices[j]<=prices[i]){
-                    ans[i] = prices[i]-prices[j];
-                    break;
+         Arrays.fill(ans, -1);
+        for(int i=prices.length-1;i>=0;i--){
+            if(st.isEmpty()){
+                ans[i] = prices[i];
+                st.push(prices[i]);
+            }
+            else{
+                if(st.peek()<=prices[i]){
+                    ans[i] = prices[i]-st.peek();
+                    st.push(prices[i]);
+
                 }
                 else{
-                    ans[i] = prices[i];
+                    while(!st.isEmpty()){
+                        if(st.peek()>prices[i]){
+                            st.pop();
+                        }
+                        else{
+                            ans[i]=prices[i]-st.peek();
+                            st.push(prices[i]);
+                            break;
+                        }
+                    }
+                    if(ans[i]==-1){
+                        ans[i]=prices[i];
+                        st.push(prices[i]);
+                    }
                 }
             }
         }
-        ans[ans.length-1] = prices[prices.length-1];
         return ans;
     }
 }
