e630fc765cd093ce88d49f5f9a41e6f680c04f36
vanshika sahu <vanshikasahu281105@gmail.com>
2025-09-02 16:54:35 +0530

    Time: 3 ms (11.68%) | Memory: 42.6 MB (38.71%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int fresh = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[][]dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j]==2){
+                    queue.add(new int[]{i,j,0});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+        int time = 0;
+        while(!queue.isEmpty()){
+            int[]arr = queue.poll();
+            int r = arr[0];
+            int c = arr[1];
+            int t = arr[2];
+            time = Math.max(time , t);
+            for(int[]d : dirs){
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                    grid[nr][nc] = 2;
+                    queue.add(new int[]{nr,  nc , t + 1});
+                    fresh--;
+                }
+
+            }
+        }
+        if(fresh > 0){
+            return -1;
+        }
+        return time;
+
+    }
+}
