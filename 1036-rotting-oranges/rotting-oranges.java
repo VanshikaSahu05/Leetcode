e293e720519e58e15d2ea90afc61464eeafdd0dd
vanshika sahu <vanshikasahu281105@gmail.com>
2025-09-06 22:39:50 +0530

    Time: 2 ms (66.40%) | Memory: 42.8 MB (29.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,79 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int fresh = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        int[][]dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(grid[i][j]==2){
-                    queue.add(new int[]{i,j,0});
+        // int n = grid.length;
+        // int m = grid[0].length;
+        // int fresh = 0;
+        // Queue<int[]> queue = new LinkedList<>();
+        // int[][]dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = 0 ; j < m ; j++){
+        //         if(grid[i][j]==2){
+        //             queue.add(new int[]{i,j,0});
+        //         }
+        //         else if(grid[i][j] == 1){
+        //             fresh++;
+        //         }
+        //     }
+        // }
+        // int time = 0;
+        // while(!queue.isEmpty()){
+        //     int[]arr = queue.poll();
+        //     int r = arr[0];
+        //     int c = arr[1];
+        //     int t = arr[2];
+        //     time = Math.max(time , t);
+        //     for(int[]d : dirs){
+        //         int nr = r + d[0];
+        //         int nc = c + d[1];
+        //         if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+        //             grid[nr][nc] = 2;
+        //             queue.add(new int[]{nr,  nc , t + 1});
+        //             fresh--;
+        //         }
+
+        //     }
+        // }
+        // if(fresh > 0){
+        //     return -1;
+        // }
+        // return time;
+
+        int row = grid.length;
+        int col = grid[0].length;
+        int fresh =0;
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] { i, j, 0 });
                 }
-                else if(grid[i][j] == 1){
+                else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
+        int[][] dir = {{0,1},{1,0},{0,-1},{-1,0}};
         int time = 0;
-        while(!queue.isEmpty()){
-            int[]arr = queue.poll();
+        while(!q.isEmpty()){
+
+           int[]arr = q.poll();
             int r = arr[0];
             int c = arr[1];
             int t = arr[2];
-            time = Math.max(time , t);
-            for(int[]d : dirs){
+            time = Math.max(time,t);
+            for(int[] d : dir){
                 int nr = r + d[0];
                 int nc = c + d[1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
+                if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1){
+                    q.add(new int[]{nr,nc,t+1});
                     grid[nr][nc] = 2;
-                    queue.add(new int[]{nr,  nc , t + 1});
                     fresh--;
                 }
-
             }
         }
-        if(fresh > 0){
+        if(fresh>0){
             return -1;
         }
         return time;
-
     }
 }
