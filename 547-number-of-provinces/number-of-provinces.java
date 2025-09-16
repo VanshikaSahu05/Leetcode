9c3ace11688e65c0d30f9406ead8717c4229d211
vanshika sahu <vanshikasahu281105@gmail.com>
2025-09-16 15:47:16 +0530

    Time: 3 ms (34.81%) | Memory: 48.2 MB (29.28%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,21 +1,56 @@
 class Solution {
-    public static void dfs(int[][] isConnected,boolean[] visited,int node){
-        visited[node] = true;
-        for(int j=0;j<isConnected[node-1].length;j++){
-            if(isConnected[node-1][j] == 1 && !visited[j+1]){
-                dfs(isConnected,visited,j+1);
-            }
+    class UnionFind
+{
+    int parent[];
+    int rank[];
+
+    UnionFind(int V) {
+        parent = new int[V+1];
+        rank = new int[V+1];
+
+        for (int i = 0; i < V; i++) {
+            parent[i]=i;
         }
     }
+
+    int find(int node){
+        if(parent[node]==node) return node;
+        return find(parent[node]);
+    }
+
+    boolean union(int u,int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu==pv) return false;
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }
+        else if(rank[pv]>rank[pu]){
+            parent[pu]=pv;
+        }
+        else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+        return true;
+    }
+    
+}
     public int findCircleNum(int[][] isConnected) {
-        boolean[] visited = new boolean[isConnected.length+1];
-        int count=0;
-        for(int i=1;i<=isConnected.length;i++){
-            if(visited[i]==false){
-                dfs(isConnected,visited,i);
-                count++;
+        int v = isConnected.length;
+        UnionFind uf = new UnionFind(v);
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                if(isConnected[i][j]==1){
+                    uf.union(i,j);
+                }
             }
         }
-        return count;
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        for (int i = 0; i < v; i++) {
+            hs.put(uf.find(i), hs.getOrDefault(uf.find(i), 0) + 1);
+        }
+        return hs.size();
     }
 }
