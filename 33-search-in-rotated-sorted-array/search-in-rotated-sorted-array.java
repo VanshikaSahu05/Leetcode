2fa666710b27288c40cfc634f87def677a757c76
vanshika sahu <vanshikasahu281105@gmail.com>
2025-09-17 22:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.12%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start =0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            //left
+            else if(arr[mid]>= arr[start]){
+                if(target>=arr[start] && target<=arr[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target>=arr[mid] && target<=arr[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
