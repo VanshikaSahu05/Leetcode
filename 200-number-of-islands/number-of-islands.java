a6099bf3eae6cf707094bbeedddd518bf527df57
vanshika sahu <vanshikasahu281105@gmail.com>
2025-09-30 01:19:57 +0530

    Time: 4 ms (49.89%) | Memory: 52.1 MB (22.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,24 +1,23 @@
 class Solution {
-    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
-    public  void dfs(char[][] grid , int r , int c){
+    public static void helper(char[][] grid,int r,int c){
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]=='0'){
+            return ;
+        }
         grid[r][c] = '0';
-        for(int[] d:dirs){
-            int row = r + d[0];
-            int col = c + d[1];
-            if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && grid[row][col]=='1'){
-                dfs(grid,row,col);
-            }
+        int[][] dir ={{0,1},{1,0},{0,-1},{-1,0}};
+        for(int i=0;i<4;i++){
+            helper(grid,r+dir[i][0],c+dir[i][1]);
         }
     }
     public int numIslands(char[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
-        int count = 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]=='1'){
+        int count=0;
+        for(int i =0 ;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == '1'){
+                    helper(grid,i,j);
                     count++;
-                    dfs(grid,i,j);
                 }
             }
         }
