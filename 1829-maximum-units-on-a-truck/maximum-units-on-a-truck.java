4e8999bda8058803fea210c9d8fb6b8aa836e7d9
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-01 14:28:18 +0530

    Time: 58 ms (5.04%) | Memory: 45.2 MB (49.09%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumUnits(int[][] arr, int t) {
+        Arrays.sort(arr,(a,b) -> (a[1]==b[1]) ? (b[0]-a[0]) : (b[1]-a[1]) );
+        int ans =0 ;
+        int ind =0;
+        while(ind < arr.length && t>0 ){
+            System.out.println("ind" + ind);
+           if(arr[ind][0]<=t){
+             ans += (arr[ind][0]*arr[ind][1]);
+            t = t-arr[ind][0];
+            ind++;
+           }
+           else{
+            ans +=(t*arr[ind][1]);
+            t=0;
+            ind++;
+           }
+           
+        }
+        return ans;
+    }
+}
