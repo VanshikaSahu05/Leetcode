be4dc827d85a909ea5700190e85a37c315ea7925
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-02 21:58:33 +0530

    Time: 2 ms (84.67%) | Memory: 45.6 MB (71.98%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean canJump(int[] nums) {
         // here we check the maxjump at every index
-        int maxInd = 0;
+        int maxInd = 0; // maxind to store the farthest index we can reach at any point. {ye bta rha ki max itne index tak aa skte hai kisi bhi point se}
         for(int i=0;i<nums.length;i++){
-            if(maxInd<i) return false;
+            if(i>maxInd) return false;
             maxInd = Math.max(maxInd,i+nums[i]);
             
         }
