2515015edb6962c3250d8322f95a88e527bca07e
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-07 21:58:40 +0530

    Time: 17 ms (36.08%) | Memory: 42.8 MB (23.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,69 +1,14 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,HashSet<Character>> hs1 = new HashMap<>();
-        HashMap<Character,HashSet<Character>> hs2 = new HashMap<>();
-
+        HashMap<Character,Character> mp = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char S = s.charAt(i);
-            char T = t.charAt(i);
-            if(hs1.containsKey(S)){
-                hs1.get(S).add(T);
-            }
-            else{
-                HashSet<Character> newHs1 = new HashSet<>();
-                newHs1.add(T);
-                hs1.put(S,newHs1);
-            }
-        }
-        for(int i=0;i<s.length();i++){
-            char S = s.charAt(i);
-            char T = t.charAt(i);
-            if(hs2.containsKey(T)){
-                hs2.get(T).add(S);
-            }
-            else{
-                HashSet<Character> newHs2 = new HashSet<>();
-                newHs2.add(S);
-                hs2.put(T,newHs2);
-            }
-        }
-        if(hs1.size()!=hs2.size()) return false;
-            
-        boolean ans1 = true, ans2 = true;
-        for(Character c:hs1.keySet()){
-            if(hs1.get(c).size()>1) ans1 = false;
-        }
-        for(Character c:hs2.keySet()){
-            if(hs2.get(c).size()>1) ans2 = false;
-        }
-        return (ans1 && ans2);
-    }
-}
-/*
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        TreeMap<Character, Integer> hs1 = new TreeMap<>();
-        TreeMap<Character, Integer> hs2 = new TreeMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            hs1.put(s.charAt(i), hs1.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        for (int i = 0; i < t.length(); i++) {
-            hs2.put(t.charAt(i), hs2.getOrDefault(t.charAt(i), 0) + 1);
-        }
-        if (hs1.size() != hs2.size()) {
-            return false;
-        }
-        Collection<Integer> values1 = hs1.values();
-        Collection<Integer> values2 = hs2.values();
-        for(int i=0;i<values1.length;i++){
-            if(values1[i]!=values2[i]){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if ((mp.containsKey(ch1) && mp.get(ch1) != ch2) || 
+                (!mp.containsKey(ch1) && mp.containsValue(ch2)))
                 return false;
-            }
+            else mp.put(ch1,ch2);
         }
         return true;
     }
 }
-*/
