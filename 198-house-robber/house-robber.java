a3f0341eae99d1d6c35edf8a3f130fab04d0aaa9
vanshika sahu <vanshikasahu281105@gmail.com>
2025-10-29 00:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (36.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,24 +1,41 @@
 class Solution {
-    public static int helper(int[] arr,int[] max ,int index,int n){
-        if(index == n){
-            return max[n-1];
+    public static int solve(int[] nums,int index,int[] dp){
+        if(index>=nums.length){
+            return 0;
         }
-        if(index == 0){
-            max[index] = arr[index];
-            return helper(arr,max,index+1,n);
+        if(dp[index]!=-1){
+            return dp[index];
         }
-        if(index == 1){
-            max[index] = Math.max(arr[0],arr[1]);
-            return helper(arr,max,index+1,n);
-        }
-        max[index] = Math.max(max[index-2]+arr[index],max[index-1]);
-        return helper(arr,max,index+1,n);
+
+        int steal = nums[index]+solve(nums,index+2,dp);
+        int skip = solve(nums,index+1,dp);
+        return dp[index]=Math.max(steal,skip);
     }
-    public int rob(int[] arr) {
-        int n = arr.length;
-        int[] max = new int[n];
-        Arrays.fill(max,-1);
-        return helper(arr,max,0,n);
-        
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return solve(nums,0,dp);
     }
 }
+/*
+class Solution {
+    public static int solve(int[] arr,int index,int[] ans,int sum){
+        if(index>=arr.length){
+            ans[0] = Math.max(ans[0],sum);
+            return ans[0];
+        }
+       solve(arr,index+2,ans,sum+arr[index]);
+       solve(arr,index+1,ans,sum);
+        return ans[0];
+    }
+    public int rob(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int[] a = new int[1];
+            a[0] = Integer.MIN_VALUE;
+            ans = Math.max(ans,solve(nums,i,a,0));
+        }
+        return ans;
+    }
+}
+*/
