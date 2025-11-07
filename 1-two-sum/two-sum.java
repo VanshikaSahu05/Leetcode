c0cb731f73f94b7b102ea4e111ea3f26a9c91ea5
vanshika sahu <vanshikasahu281105@gmail.com>
2025-11-08 00:58:39 +0530

    Time: 2 ms (99.11%) | Memory: 46.8 MB (5.61%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,56 +1,15 @@
-
-// import java.util.Arrays;
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int k = 0;
-//         for(int i=0; i<nums.length; i++){
-//             for(int j=1; j<nums.length; j++){
-//                 if(nums[i]+nums[j]==target){
-//                     int arr[k++] = i;
-//                     arr[k] = j;
-//                     break;
-//                     }
-//                 }
-//         }
-
-//         
-//     }
-//     
-// }
-// import java.util.Arrays;
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         // int  k= 0;
-//         int arr[] = new int[2];
-//         for(int i=0; i<nums.length; i++){
-//             for(int j=i+1; j<nums.length; j++){
-//                 if(nums[i]+nums[j]==target){
-//                     arr[0] = i;
-//                     arr[1] = j;
-//                     return arr;
-//                     }
-//                 }
-//             }
-//     return arr;
-//     }
-// }
-import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        ArrayList<Integer> l = new ArrayList<>();
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    l.add(i);
-                    l.add(j);
-                    }
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int tar = target - nums[i];
+            if(hs.containsKey(tar)){
+                return new int[]{hs.get(tar),i};
+            }
+            else{
+                hs.put(nums[i],i);
             }
         }
-        int[] arr= new int[l.size()];
-        for(int k=0;k<l.size();k++){
-            arr[k]=l.get(k);
-        }
-        return arr;
-        
+        return new int[]{-1,-1};
     }
 }
