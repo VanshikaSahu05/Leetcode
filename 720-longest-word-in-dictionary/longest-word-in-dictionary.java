6aadbad7cb8339fc78ccce60a56c7afdaf24e209
vanshika sahu <vanshikasahu281105@gmail.com>
2025-12-19 22:14:27 +0530

    Time: 17 ms (52.17%) | Memory: 46.6 MB (39.79%) - LeetSync

---
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        String s="";
+        Set<String> set=new HashSet<>();
+        for(String cur:words){
+           if(cur.length()==1||set.contains(cur.substring(0,cur.length()-1))){
+               set.add(cur);
+               if(cur.length() > s.length())s=cur;
+           }        
+        }
+        return s;
+    }
+}
