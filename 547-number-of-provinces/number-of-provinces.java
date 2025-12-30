dac7ba8608d6dc24357b06c2d257e8b5761b2104
vanshika sahu <vanshikasahu281105@gmail.com>
2025-12-30 12:56:57 +0530

    Time: 3 ms (33.10%) | Memory: 49.3 MB (13.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,56 +1,34 @@
 class Solution {
-    class UnionFind
-{
-    int parent[];
-    int rank[];
-
-    UnionFind(int V) {
-        parent = new int[V+1];
-        rank = new int[V+1];
-
-        for (int i = 0; i < V; i++) {
-            parent[i]=i;
-        }
-    }
-
-    int find(int node){
-        if(parent[node]==node) return node;
-        return find(parent[node]);
-    }
-
-    boolean union(int u,int v){
-        int pu = find(u);
-        int pv = find(v);
-
-        if(pu==pv) return false;
-        if(rank[pu]>rank[pv]){
-            parent[pv]=pu;
-        }
-        else if(rank[pv]>rank[pu]){
-            parent[pu]=pv;
-        }
-        else{
-            parent[pv]=pu;
-            rank[pu]++;
-        }
-        return true;
-    }
-    
-}
     public int findCircleNum(int[][] isConnected) {
-        int v = isConnected.length;
-        UnionFind uf = new UnionFind(v);
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
-                if(isConnected[i][j]==1){
-                    uf.union(i,j);
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        int vertex = isConnected.length;
+        for(int i=0;i<vertex;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0;i<isConnected.length;i++){
+            for(int j=0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
                 }
             }
         }
-        HashMap<Integer, Integer> hs = new HashMap<>();
-        for (int i = 0; i < v; i++) {
-            hs.put(uf.find(i), hs.getOrDefault(uf.find(i), 0) + 1);
+        int count=0;
+        boolean[] visited = new boolean[vertex];
+        for(int i=0;i<vertex;i++){
+            if(visited[i]!=true){
+                dfs(visited,graph,i);
+                count++;
+            }
         }
-        return hs.size();
+        return count;
+    }
+    public static void dfs(boolean[] visited,ArrayList<ArrayList<Integer>> graph,int node){
+        visited[node]=true;
+        for(int n:graph.get(node)){
+            if(visited[n]!=true){
+                dfs(visited,graph,n);
+            }
+        }
     }
 }
